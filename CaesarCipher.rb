caa4f4650ee2eb596a9cfeb5314b19ba17a4398b
thekairath <thekairath@hotmail.com>
2015-06-01 17:03:15 +0300

    Update CaesarCipher.rb

--- CaesarCipher.rb
+++ CaesarCipher.rb
@@ -1,1 +1,22 @@
 
+def ccipher(w,n)
+  n_word = ""
+  w.each_char do |i|
+    n.times do
+      if(i == "z")
+        i= "a"
+        next
+      elsif(i == "Z")
+        i = "A"
+        next
+      end
+      i.next!
+      i =="%" ? i = " " : ""
+    end
+    n_word += i
+  end
+  puts n_word
+end
+
+
+ccipher("What a string!", 5)
